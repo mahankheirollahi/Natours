44cd5de67bb7395905fb99e590825528d813e707
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2024-07-06 21:35:34 +0300

    Refactor server file

--- server.js
+++ server.js
@@ -15,40 +15,7 @@ mongoose
     useFindAndModify: false,
     useUnifiedTopology: true,
   })
-  .then(() => console.log('DB connection successful'));
-
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-});
-
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-  name: 'The Forest Hike 2',
-  rating: 4.7,
-  price: 200,
-});
-
-testTour
-  .save()
-  .then((doc) => {
-    console.log(doc);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+  .then(() => console.log('DB connection successful 🚀'));
 
 const port = process.env.PORT || 3000;
 
